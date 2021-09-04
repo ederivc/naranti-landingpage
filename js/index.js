0d9cff91d7192f19cdd8116b421d0832b0ba7631
ederivc <ederivc@gmail.com>
2021-09-04 01:45:38 -0500

    add questions section and modified hero section

--- js/index.js
+++ js/index.js
@@ -1,9 +1,14 @@
 const btn = document.querySelector(".menu-toggle");
 const links = document.querySelector(".navbar__menu");
 const navbar = document.querySelector(".navbar");
+const title = document.getElementById("web-title");
+const webTitle = "HACEMOS VISIBLES TUS SUEÑOS";
 
 btn.addEventListener("click", () => {
   btn.classList.toggle("is-active");
   navbar.classList.toggle("navbar-active");
   links.classList.toggle("active");
 });
+
+title.innerHTML = webTitle;
+title.style.setProperty("--characters", webTitle.length);
